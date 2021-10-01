9bc4f1512f8784defc26677f34f095c35bdb4c43
bartek.skorecki <bartek.skorecki@wrxflo.com>
2021-10-02 00:03:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,103 @@
 import time
+
 import vtk
+from PIL import Image
+import matplotlib.pyplot as plt
+
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+iren = vtk.vtkRenderWindowInteractor()
+x = []
+y = []
 
 
-class Model:
-    def __init__(self):
-        self.ren = vtk.vtkRenderer()
-        self.renWin = vtk.vtkRenderWindow()
-        self.screenshot_count = 0
-        self.colors = vtk.vtkNamedColors()
+def render():
+    colors = vtk.vtkNamedColors()
 
-    def stlToActor(self, filename):
-        # Create STL reader
-        reader = vtk.vtkSTLReader()
-        reader.SetFileName(filename)
+    filename = "asteroid.stl"
 
-        # Create map to convert STL to Polydata
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName(filename)
 
-        # Use above map to convert STL to an Actor
-        actor = vtk.vtkActor()
-        actor.SetMapper(mapper)
-        # Set the actor colour
-        actor.GetProperty().SetDiffuseColor(self.colors.GetColor3d('Light_Grey'))
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(reader.GetOutputPort())
 
-        return actor
+    polydata = reader.GetOutput()
+    print(polydata)
 
-    @staticmethod
-    def createLightSource(x, y, z):
-        light = vtk.vtkLight()
-        light.SetIntensity(1)
-        light.SetPosition(x, y, z)
-        light.SetDiffuseColor(1, 1, 1)
-        return light
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetDiffuseColor(colors.GetColor3d('Light_Grey'))
 
-    def render(self):
-        # Create a rendering window and renderer
-        self.renWin.AddRenderer(self.ren)
-        self.renWin.SetWindowName('ReadSTL')
+    # Create a rendering window and renderer
+    renWin.AddRenderer(ren)
+    renWin.SetWindowName('ReadSTL')
 
-        # Assign actor to the renderer
-        actor = self.stlToActor("asteroidLow.stl")
-        self.ren.AddActor(actor)
-        self.ren.SetBackground(self.colors.GetColor3d('Black'))
+    # Create a renderwindowinteractor
+    iren.SetRenderWindow(renWin)
 
-        # Add an external light ("The sun")
-        self.ren.AddLight(self.createLightSource(5, -5, 5))
+    # Assign actor to the renderer
+    ren.AddActor(actor)
+    ren.SetBackground(colors.GetColor3d('Black'))
 
-        # Render scene
-        self.renWin.Render()
+    light = vtk.vtkLight()
+    light.SetIntensity(5)
+    light.SetPosition(1, -1, 1)
+    light.SetDiffuseColor(1, 1, 1)
+    ren.AddLight(light)
+    plt.ylabel('Lumens per (square meter)')
+    # Enable user interface
+    iren.Initialize()
+    renWin.Render()
+    camera = ren.GetActiveCamera()
+    # iren.Start()
+    transform = vtk.vtkTransform()
+    for i in range(360):
+        actor.RotateY(1)
+        # Roll(camera)
+        screenshot(i)
+        renWin.Render()
 
-    def generateLightShots(self, count):
-        for i in range(count):
-            self.roll()
-            self.screenshot(i)
-            self.renWin.Render()
-
-    def roll(self):
-        # Get the current camera
-        camera = self.ren.GetActiveCamera()
-
-        # Create a rotation
-        transform = vtk.vtkTransform()
-        transform.Identity()
-        transform.RotateWXYZ(1, 1, 1, 1)
-
-        # Apply the rotation
-        camera.ApplyTransform(transform)
-        camera.OrthogonalizeViewUp()
-        self.ren.ResetCameraClippingRange()
-
-    def screenshot(self, count, filename=None):
-        # Create image filter
-        w2if = vtk.vtkWindowToImageFilter()
-        w2if.SetInput(self.renWin)
-        w2if.Update()
-
-        # Handle default file name
-        if filename is None:
-            filename = 'screenshot'
-        filename = filename + '%d.png' % count
-
-        # Generate and write the image
-        writer = vtk.vtkPNGWriter()
-        writer.SetFileName(filename)
-        writer.SetInputData(w2if.GetOutput())
-        writer.Write()
+    plt.plot(x, y)
+    plt.show()
 
 
-scene = Model()
-scene.render()
-scene.generateLightShots(10)
+def Roll(camera):
+    transform = vtk.vtkTransform()
+    transform.Identity()
+    transform.RotateWXYZ(1, 1, 1, 1)
+    camera.ApplyTransform(transform)
+    camera.OrthogonalizeViewUp()
+    ren.ResetCameraClippingRange()
+
+
+def screenshot(screenshot_count):
+    w2if = vtk.vtkWindowToImageFilter()
+    w2if.SetInput(renWin)
+    w2if.Update()
+    filename = 'screenshot.png'
+    writer = vtk.vtkPNGWriter()
+    writer.SetFileName(filename)
+    writer.SetInputData(w2if.GetOutput())
+    writer.Write()
+    print(" ")
+    image = Image.open(filename)
+    print("%s\t%s" % (filename, calculate_brightness(image)))
+    y.append(calculate_brightness(image))
+    x.append(screenshot_count)
+
+
+def calculate_brightness(image):
+    greyscale_image = image.convert('L')
+    histogram = greyscale_image.histogram()
+    pixels = sum(histogram)
+    brightness = scale = len(histogram)
+
+    for index in range(0, scale):
+        ratio = histogram[index] / pixels
+        brightness += ratio * (-scale + index)
+
+    return 1 if brightness == 255 else brightness / scale
+
+
+render()
