b77c07c2a57eed0d703221197aae3a90ed98f3b2
MrBaconMajster <48122592+MrBaconMajster@users.noreply.github.com>
2021-10-02 21:49:45 +0100

    Add files via upload

--- mainMenu.py
+++ mainMenu.py
@@ -14,7 +14,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(467, 502)
+        Form.resize(564, 650)
+        Form.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0.388, y1:1, x2:1, y2:0, stop:0 rgba(0, 0, 0, 255), stop:0.995025 rgba(0, 0, 120, 255));")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.verticalLayout = QtWidgets.QVBoxLayout()
@@ -22,10 +23,25 @@ class Ui_Form(object):
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem)
         self.label = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setFamily("Corbel")
+        font.setPointSize(32)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgba(255, 255, 255, 0);")
+        self.label.setTextFormat(QtCore.Qt.RichText)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem1)
         self.label_2 = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setFamily("Corbel")
+        font.setPointSize(10)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgba(255, 255, 255, 0);")
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
         self.verticalLayout.addWidget(self.label_2)
@@ -33,16 +49,26 @@ class Ui_Form(object):
         self.horizontalLayout.setContentsMargins(-1, -1, -1, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.comboBox = QtWidgets.QComboBox(Form)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
         self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setMinimumSize(QtCore.QSize(255, 0))
+        font = QtGui.QFont()
+        font.setFamily("Corbel")
+        font.setPointSize(10)
+        self.comboBox.setFont(font)
+        self.comboBox.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(185, 185, 185);\n"
+"border: 1px solid gray;\n"
+"      border-radius: 3px;")
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.horizontalLayout.addWidget(self.comboBox)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setContentsMargins(-1, 10, -1, -1)
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         self.startSim_Button = QtWidgets.QPushButton(Form)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Fixed)
@@ -50,26 +76,60 @@ class Ui_Form(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.startSim_Button.sizePolicy().hasHeightForWidth())
         self.startSim_Button.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.startSim_Button.setFont(font)
+        self.startSim_Button.setStyleSheet("background-color: rgb(185, 185, 185);\n"
+"    border-style: outset;\n"
+"    border-width: 2px;\n"
+"    border-radius: 10px;\n"
+"    border-color: rgb(143, 143, 143);\n"
+"    font: bold 14px;\n"
+"    min-width: 10em;\n"
+"    padding: 6px;\n"
+"\n"
+"color: rgb(255, 255, 255);")
         self.startSim_Button.setObjectName("startSim_Button")
         self.horizontalLayout_3.addWidget(self.startSim_Button)
         self.verticalLayout.addLayout(self.horizontalLayout_3)
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem1)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem2)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_2.addItem(spacerItem2)
+        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem3)
         self.stlDir_LinerEdit = QtWidgets.QLineEdit(Form)
         self.stlDir_LinerEdit.setMinimumSize(QtCore.QSize(300, 0))
+        self.stlDir_LinerEdit.setStyleSheet("background-color: rgba(255, 255, 255, 255);")
         self.stlDir_LinerEdit.setObjectName("stlDir_LinerEdit")
         self.horizontalLayout_2.addWidget(self.stlDir_LinerEdit)
         self.importSTL_Button = QtWidgets.QPushButton(Form)
+        font = QtGui.QFont()
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.importSTL_Button.setFont(font)
+        self.importSTL_Button.setStyleSheet("background-color: rgb(185, 185, 185);\n"
+"    border-style: outset;\n"
+"    border-width: 2px;\n"
+"    border-radius: 10px;\n"
+"    border-color: rgb(143, 143, 143);\n"
+"    font: bold 14px;\n"
+"    min-width: 10em;\n"
+"    padding: 6px;\n"
+"\n"
+"color: rgb(255, 255, 255);")
         self.importSTL_Button.setObjectName("importSTL_Button")
         self.horizontalLayout_2.addWidget(self.importSTL_Button)
-        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_2.addItem(spacerItem3)
+        spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem4)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setContentsMargins(-1, 10, -1, -1)
         self.horizontalLayout_4.setObjectName("horizontalLayout_4")
         self.startCustomSim_Button = QtWidgets.QPushButton(Form)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Fixed)
@@ -77,11 +137,58 @@ class Ui_Form(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.startCustomSim_Button.sizePolicy().hasHeightForWidth())
         self.startCustomSim_Button.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.startCustomSim_Button.setFont(font)
+        self.startCustomSim_Button.setStyleSheet("background-color: rgb(185, 185, 185);\n"
+"    border-style: outset;\n"
+"    border-width: 2px;\n"
+"    border-radius: 10px;\n"
+"    border-color: rgb(143, 143, 143);\n"
+"    font: bold 14px;\n"
+"    min-width: 10em;\n"
+"    padding: 6px;\n"
+"\n"
+"color: rgb(255, 255, 255);")
         self.startCustomSim_Button.setObjectName("startCustomSim_Button")
         self.horizontalLayout_4.addWidget(self.startCustomSim_Button)
         self.verticalLayout.addLayout(self.horizontalLayout_4)
-        spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem4)
+        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem5)
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setMaximumSize(QtCore.QSize(100, 100))
+        self.label_4.setText("")
+        self.label_4.setPixmap(QtGui.QPixmap("../unnamed.jpg"))
+        self.label_4.setScaledContents(True)
+        self.label_4.setObjectName("label_4")
+        self.horizontalLayout_5.addWidget(self.label_4)
+        self.label_3 = QtWidgets.QLabel(Form)
+        font = QtGui.QFont()
+        font.setFamily("Corbel")
+        self.label_3.setFont(font)
+        self.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgba(255, 255, 255, 0);")
+        self.label_3.setWordWrap(True)
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout_5.addWidget(self.label_3)
+        spacerItem6 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_5.addItem(spacerItem6)
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setMaximumSize(QtCore.QSize(100, 100))
+        self.label_5.setText("")
+        self.label_5.setPixmap(QtGui.QPixmap("../Space_Apps_Logo_Black (1).jpeg"))
+        self.label_5.setScaledContents(True)
+        self.label_5.setObjectName("label_5")
+        self.horizontalLayout_5.addWidget(self.label_5)
+        self.verticalLayout.addLayout(self.horizontalLayout_5)
+        self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
+        self.verticalLayout.addLayout(self.horizontalLayout_6)
         self.verticalLayout_2.addLayout(self.verticalLayout)
 
         self.retranslateUi(Form)
@@ -90,12 +197,13 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.label.setText(_translate("Form", "Space Beans"))
+        self.label.setText(_translate("Form", "Light Curve Visualiser"))
         self.label_2.setText(_translate("Form", "Choose object shape"))
         self.comboBox.setItemText(0, _translate("Form", "Bennu Asteroid"))
         self.startSim_Button.setText(_translate("Form", "Start Simulation"))
         self.importSTL_Button.setText(_translate("Form", "Import Custom STL"))
         self.startCustomSim_Button.setText(_translate("Form", "Start Custom Simulation"))
+        self.label_3.setText(_translate("Form", "Coded by team Space Bean Hunters for NASA 2021 Limerick Space Apps Challenge"))
 
 
 if __name__ == "__main__":
